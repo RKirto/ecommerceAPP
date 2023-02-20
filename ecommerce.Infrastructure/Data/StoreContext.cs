09d431c4503e3e64038d5102321226d6c8dff12d
Rati Kirtadze <rkirto1@gmail.com>
2023-02-21 01:10:01 +0400

    dev

--- ecommerce.Infrastructure/Data/StoreContext.cs
+++ ecommerce.Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using ecommerce.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,4 +10,9 @@ public class StoreContext : DbContext
     public DbSet<Product> Products { get; set; }
     public DbSet<ProductType> ProductTypes { get; set; }
     public DbSet<ProductBrand> ProductBrands { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
