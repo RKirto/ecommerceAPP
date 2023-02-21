9df7e1317d242dd6f9d07aefe435dca03b4eb19f
Rati Kirtadze <rkirto1@gmail.com>
2023-02-21 16:28:47 +0400

    dev

--- ecommerce.API/Program.cs
+++ ecommerce.API/Program.cs
@@ -24,6 +24,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+    try {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    } 
+    catch (Exception ex) 
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "an error occured during migration");
+    }
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
